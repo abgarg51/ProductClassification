9a68227eb80e031830435c861776b490829538f4
Steven <steven@Stevens-MacBook-Pro.local>
2014-11-01 12:09:06 -0700

    rollup catagory in data parser

--- parse_amazon_reviews.py
+++ parse_amazon_reviews.py
@@ -12,8 +12,11 @@ import io
 # Warning: the script is silentlly discarding mis-formated Items.
 ######################################
 
-def getListOfNodeIDForRootParent(rootParentName):
+def getCatgoryDictForRootParent(rootParentName):
 	nodeIds = {}
+	nodeIdToCatgoryDict = {}
+	catogoryIdToNameDict = {}
+	conflicts = []
 	with open('./data/AmazonHeirarchy.json', 'r+') as data:
 		jsons = []
 		for line in data:
@@ -21,8 +24,13 @@ def getListOfNodeIDForRootParent(rootParentName):
 			jsons.append(jsonObj)
 		
 
-		def getChildren(subTree):
-			nodeIds[subTree['BrowseNodeId']] = subTree['Name']
+		def getChildren(subTree, catId):
+			nodeID = int(subTree['BrowseNodeId']) 
+			nodeIds[nodeID] = subTree['Name']
+			if (nodeIdToCatgoryDict.has_key(nodeID) and (nodeIdToCatgoryDict[nodeID] != catId)):
+				conflicts.append((nodeID, catId, nodeIdToCatgoryDict[nodeID]))
+			else:
+				nodeIdToCatgoryDict[nodeID] = catId
 			if not subTree.has_key('Children'):
 				#print "No children for: ", subTree
 				return
@@ -31,15 +39,32 @@ def getListOfNodeIDForRootParent(rootParentName):
 				children = [children]
 			for child in children:
 				if type(child) is dict:
-					getChildren(child)
+					getChildren(child, catId)
 			return children
 
 		for tree in jsons[0]:
 			if tree['Name'] == rootParentName:
-				nodeIds[tree['BrowseNodeId']] = tree['Name']
+				nodeID = int(tree['BrowseNodeId']) 
+				nodeIds[nodeID] = tree['Name']
+				nodeIdToCatgoryDict[nodeID] = nodeID
 				for child in tree['Children']:
-					getChildren(child)
-	return nodeIds
+					catId = child['BrowseNodeId']
+					catName = child['Name']
+					catogoryIdToNameDict[catId] = catName
+					getChildren(child, catId)
+
+		#print catogoryIdToNameDict
+		#print nodeIds
+		#print nodeIdToCatgoryDict
+		print "nodeID count in dict: ", len(nodeIdToCatgoryDict)
+		print "nodeID count in list: ", len(nodeIds)
+		#print "Conflicts: ========================"
+		#for conflict in conflicts:
+		#	print "Conflict: (NodeID, new catgoryID, old category ID) = ", conflict
+		for nodeID in nodeIds:
+			if (not nodeIdToCatgoryDict.has_key):
+				print "Missing nodeID from dict: ",nodeID
+	return nodeIdToCatgoryDict
 
 
 
@@ -49,7 +74,7 @@ outFile = io.open("./data/amazon_review.txt", "w", encoding='utf-8')
 
 with open('./data/amazon_products', 'r+') as data_file:
 	jsons = []
-	appliancesNodeIds = getListOfNodeIDForRootParent('Appliances')
+	nodeIdToCatgoryDict = getCatgoryDictForRootParent('Appliances')
 	productCount = 0
 	reviewCount = 0
 	applianceCount = 0
@@ -68,15 +93,19 @@ with open('./data/amazon_products', 'r+') as data_file:
 		if type(browseNodes) is not list:
 			browseNodes = [browseNodes]
 		browseNodeIDs = [node['BrowseNodeId'] for node in browseNodes if node.has_key('BrowseNodeId')]
-		for nodeID in browseNodeIDs:
-			if nodeID not in appliancesNodeIds.keys():
-				browseNodeIDs.remove(nodeID)
+		#filter out none appliance IDs.
+		browseNodeIDs = [node for node in browseNodeIDs if node in nodeIdToCatgoryDict.keys()]
+		#skip if no ID is left
 		if len(browseNodeIDs) > 0:
 			applianceCount += 1
+		else:
+			continue
+		#start to print
 		for text in reviewTexts:
 			reviewCount += 1
 			for ID in browseNodeIDs:
-				out_obj = {'review':text, 'id':ID}
+				#put in catogory ID instead of the browsnodeID of the node.
+				out_obj = {'review':text, 'id':nodeIdToCatgoryDict[ID]}
 				outFile.write(unicode(json.dumps(out_obj, ensure_ascii=False)))
 				outFile.write(unicode('\n'))
 
