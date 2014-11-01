ee1c808d7c1b8ff2ede7e4113bddd88619822cbb
Norman S Yu <normanyu@fb.com>
2014-11-01 13:51:46 -0700

    Changed to TFIDF

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__  == '__main__':
 	print 'Extracting features...'
 
 	# use BagOfWords with TFIDF normalizationn
-	vectorizer = features.CountVectorizer(min_df=1)
+	vectorizer = features.TfidfVectorizer(min_df=1)
 	# put features into a pandas dataframe and fill 0 entries
 	X = vectorizer.fit_transform(corpus)
 	# set y to targets nodes
