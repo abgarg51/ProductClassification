0232a9ff21b0cf86c2f9342c650bef38e1f28216
Steven Fu <stevenfu@stanford.edu>
2014-11-06 14:01:01 -0800

    created amazon category name file

---
+++ amazone_tree_parser.py
@@ -0,0 +1,101 @@
+import json
+import io
+import sys
+from collections import Counter
+
+def loadAmazoneHeirarchyTree(treeName):
+	with open(treeName, 'r+') as data:
+		jsons = []
+		for line in data:
+			jsonObj = json.loads(line)
+			jsons.append(jsonObj)
+		amazoneTrees = jsons[0]
+	return amazoneTrees
+
+
+
+def getRootCatgoryNames(amazoneTrees):
+	rootCatgoryNames = []
+	for tree in amazoneTrees:
+		rootCatgoryNames.append(tree['Name'])
+	return rootCatgoryNames
+
+def printRootCatgoryNames(rootCatgoryNames):
+	for i in range(len(rootCatgoryNames)):
+		print "Index: ",i, "\t",rootCatgoryNames[i]
+
+def getCatgoryNamesForRootParent(trees,rootParentName):
+	nodeIds = {}
+	nodeIdToCatgoryDict = {}
+	catogoryIdToNameDict = {}
+	conflicts = []
+	#recursive sub routine to search down the tree.
+	def getChildren(subTree, catId):
+		nodeID = int(subTree['BrowseNodeId']) 
+		nodeIds[nodeID] = subTree['Name']
+		if (nodeIdToCatgoryDict.has_key(nodeID) and (nodeIdToCatgoryDict[nodeID] != catId)):
+			conflicts.append((nodeID, catId, nodeIdToCatgoryDict[nodeID]))
+		else:
+			nodeIdToCatgoryDict[nodeID] = catId
+		if not subTree.has_key('Children'):
+			#print "No children for: ", subTree
+			return
+		children = subTree['Children']
+		if type(children) is not list:
+			children = [children]
+		for child in children:
+			if type(child) is dict:
+				getChildren(child, catId)
+		return children
+			
+		#start from the root, use first level children as rollup catagories.
+	for tree in trees:
+		if tree['Name'] == rootParentName:
+			nodeID = int(tree['BrowseNodeId']) 
+			nodeIds[nodeID] = tree['Name']
+			nodeIdToCatgoryDict[nodeID] = nodeID
+			if not tree.has_key('Children'):
+				continue
+			for child in tree['Children']:
+				catId = child['BrowseNodeId']
+				catName = child['Name']
+				catogoryIdToNameDict[catId] = catName
+				getChildren(child, catId)
+
+	#print "CategoryID to Name mapping FYI:"
+	#print catogoryIdToNameDict
+	#print nodeIds
+	#print nodeIdToCatgoryDict
+	#print "nodeID count in dict: ", len(nodeIdToCatgoryDict)
+	#print "nodeID count in list: ", len(nodeIds)
+	#print "Conflicts: ========================"
+	#for conflict in conflicts:
+	#	print "Conflict: (NodeID, new catgoryID, old category ID) = ", conflict
+	for nodeID in nodeIds:
+		if (not nodeIdToCatgoryDict.has_key):
+			print "Missing nodeID from dict: ",nodeID
+	return catogoryIdToNameDict
+
+if __name__ == "__main__":
+	amazoneTrees = loadAmazoneHeirarchyTree('./data/AmazonHeirarchy.json')
+	ouputFileName = "./data/amazonCategoryNames.txt"
+	rootCatgoryNames = getRootCatgoryNames(amazoneTrees)
+	outFile = io.open(ouputFileName, "w", encoding='utf-8')
+
+	outputDict = {}
+
+	for cat in rootCatgoryNames:
+		names = getCatgoryNamesForRootParent(amazoneTrees, cat)
+		outputDict[cat] = names
+
+	outFile.write(unicode(json.dumps(outputDict, ensure_ascii=False)))
+	#outFile.write(unicode('\n'))
+
+	print "amazone category information exported to: ", ouputFileName
+	print "Format:"
+	print "it's a Json of a dict, keys are root category names. Value is another Dict."
+	print "Value dict has catgoryID as keys, and names as values"
+
+	#print outputDict
+
+
