dacb00083e07ebc6b813d55c00c4fae1d93304a2
Norman S Yu <normanyu@fb.com>
2014-11-15 18:18:26 -0800

    Input format

--- hdp_example.py
+++ hdp_example.py
@@ -5,13 +5,14 @@ all_sources = ['amazon']
 tree_category = 'books'
 corpus, y = util.read_from_sources(all_sources, tree_category = tree_category, max_examples = 5000)
 
-reviews = corpus[all_sources[0]]
-
 cleaned_reviews = [util.clean_sentence(review.encode('ascii', 'ignore')) for review in reviews]
 
-dictionary = corpora.Dictionary(cleaned_reviews)
+# gensim expects each example to be a list of words, instead of a long string
+texts = [[word for word in r.split()] for r in cleaned_reviews]
 
-corpus_bow = [dictionary.doc2bow(review) for review in cleaned_reviews]
+dictionary = corpora.Dictionary(texts)
+
+corpus_bow = [dictionary.doc2bow(text) for text in texts]
 
 model = models.hdpmodel.HdpModel(corpus_bow, id2word=dictionary)
 
