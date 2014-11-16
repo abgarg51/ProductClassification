285623423083c038b70737000f3d65aa1d471047
Norman S Yu <normanyu@fb.com>
2014-11-15 19:02:15 -0800

    Got rid of inference

--- hdp_example.py
+++ hdp_example.py
@@ -5,6 +5,8 @@ all_sources = ['amazon']
 tree_category = 'books'
 corpus, y = util.read_from_sources(all_sources, tree_category = tree_category, max_examples = 5000)
 
+reviews = corpus[all_sources[0]]
+
 cleaned_reviews = [util.clean_sentence(review.encode('ascii', 'ignore')) for review in reviews]
 
 # gensim expects each example to be a list of words, instead of a long string
@@ -14,8 +16,17 @@ dictionary = corpora.Dictionary(texts)
 
 corpus_bow = [dictionary.doc2bow(text) for text in texts]
 
-model = models.hdpmodel.HdpModel(corpus_bow, id2word=dictionary)
+tfidf = models.TfidfModel(corpus_bow)
+corpus_tfidf = tfidf[corpus_bow]
 
-corpus_eval =  model.inference(corpus_bow)
+# don't use this - HPD with TFIDF with crash your computer
+# model = models.hdpmodel.HdpModel(corpus_bow, id2word=dictionary, T = 20)
 
-model.show_topics(100)
+model = models.ldamodel.LdaModel(corpus_tfidf, id2word=dictionary, num_topics = 20)
+
+topic_distributions = list(model[corpus_tfidf])
+
+# print_topic(topicid, topn=10)¶
+model.print_topic(10)
+
+#$print_topics(num_topics=10, num_words=10)
